dc7893a42dbe5cfe19638694eb1d6cf97a93dfca
ferhatucar <ucarferhat3@gmail.com>
2019-10-31 10:47:14 +0100

    updates user component with type checking, adds spinner and search component

---
+++ src/components/layout/Spinner.js
@@ -0,0 +1,17 @@
+import React, {Fragment} from 'react';
+import spinner from './spinner.gif'
+
+const Spinner = () =>
+    <Fragment>
+        <img src={spinner} alt="Loading..." style={spinnerStyle}/>
+    </Fragment>
+;
+
+// styles
+const spinnerStyle = {
+    display: 'block',
+    margin: 'auto',
+    width: '200px'
+};
+
+export default Spinner;
