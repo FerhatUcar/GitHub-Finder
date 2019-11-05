f55a9b10bddc8632cc620c6d0d96191be1f3a38f
ferhatucar <ucarferhat3@gmail.com>
2019-11-05 13:05:19 +0100

    destructuring components and changes to func components

--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -1,64 +1,56 @@
-import React, {Component} from 'react';
+import React, {useState} from 'react';
 import PropTypes from "prop-types";
 
-class Search extends Component {
-    state = {
-        text: ''
-    };
-
-    // type checking
-    static propTypes = {
-        searchUsers: PropTypes.func.isRequired,
-        clearUsers: PropTypes.func.isRequired,
-        setAlert: PropTypes.func.isRequired
-    };
+const Search = ({showClear, clearUsers, searchUsers, setAlert}) => {
+    const [text, setText] = useState('');
 
     // get the value from the input
-    onChange = (e): void => this.setState({[e.target.name]: e.target.value});
+    const onChange = (e): void => setText(e.target.value);
 
     // submit the current value
-    onSubmit = (e): void => {
+    const onSubmit = (e): void => {
         e.preventDefault();
 
-        if (this.state.text === '') {
-            this.props.setAlert('Please enter text', 'danger');
-        } else {
-            this.props.searchUsers(this.state.text);
-            this.setState({text: ''})
-        }
+        if (text === '') setAlert('Please enter text', 'danger');
+
+        searchUsers(text).then();
+        setText('')
     };
 
-    render() {
-        const {showClear, clearUsers} = this.props;
-
-        return (
-            <div className="search__box">
-                <div className="card card__space">
-                    <h1>Search for Github users</h1>
-                    <p>Type the users name in the search box to get results</p>
-                </div>
-                <form onSubmit={this.onSubmit} className="form">
-                    <input
-                        type="text"
-                        name="text"
-                        value={this.state.text}
-                        onChange={this.onChange}
-                        placeholder="Search" />
-                    <input
-                        type="submit"
-                        value="Search"
-                        className="btn btn-dark btn-block"
-                    />
-                    {showClear && (
-                        <button
-                            className="btn btn-light btn-block"
-                            onClick={clearUsers}
-                        >Clear</button>
-                    )}
-                </form>
+    return (
+        <div className="search__box">
+            <div className="card card__space">
+                <h1>Search for Github users</h1>
+                <p>Type the users name in the search box to get results</p>
             </div>
-        );
-    }
-}
+            <form onSubmit={onSubmit} className="form">
+                <input
+                    type="text"
+                    name="text"
+                    value={text}
+                    onChange={onChange}
+                    placeholder="Search" />
+                <input
+                    type="submit"
+                    value="Search"
+                    className="btn btn-dark btn-block"
+                />
+                {showClear && (
+                    <button
+                        className="btn btn-light btn-block"
+                        onClick={clearUsers}
+                    >Clear</button>
+                )}
+            </form>
+        </div>
+    );
+};
+
+// type checking
+Search.propTypes = {
+    searchUsers: PropTypes.func.isRequired,
+    clearUsers: PropTypes.func.isRequired,
+    setAlert: PropTypes.func.isRequired
+};
 
 export default Search;
--- src/components/users/User.js
+++ src/components/users/User.js
@@ -1,140 +1,140 @@
-import React, {Component, Fragment} from 'react';
+import React, {useEffect, Fragment} from 'react';
 import Spinner from "../layout/Spinner";
 import PropTypes from "prop-types";
 import {Link} from "react-router-dom";
 import Repos from "../repos/Repos";
 
-class User extends Component {
-    componentDidMount(): void {
-        this.props.getUser(this.props.match.params.login).then();
-        this.props.getUserRepos(this.props.match.params.login).then();
-    }
+const User = ({loading, repos, user, getUser, getUserRepos, match}) => {
+    useEffect(() => {
+        getUser(match.params.login);
+        getUserRepos(match.params.login);
+        // eslint-disable-next-line
+    }, []);
 
-    static propTypes = {
-        loading: PropTypes.bool,
-        user: PropTypes.object.isRequired,
-        repos: PropTypes.array.isRequired,
-        getUser: PropTypes.func.isRequired,
-        getUserRepos: PropTypes.func.isRequired
-    };
+    const {
+        name,
+        avatar_url,
+        company,
+        location,
+        bio,
+        blog,
+        email,
+        login,
+        html_url,
+        followers,
+        following,
+        public_repos,
+        public_gists,
+        hireable
+    } = user;
 
-    render() {
-        const {
-            name,
-            avatar_url,
-            company,
-            location,
-            bio,
-            blog,
-            email,
-            login,
-            html_url,
-            followers,
-            following,
-            public_repos,
-            public_gists,
-            hireable
-        } = this.props.user;
 
-        const {loading ,repos} = this.props;
+    // show loader if the data is not been fetched
+    if (loading) return <Spinner/>;
 
-        // show loader if the data is not been fetched
-        if (loading) return <Spinner/>;
-
-        return (
-            <Fragment>
-                <div className="card flex">
-                    <div className="all-center card__space">
+    return (
+        <Fragment>
+            <div className="card flex">
+                <div className="all-center card__space">
+                    <div>
+                        <img
+                            src={avatar_url}
+                            alt={name}
+                            className="round-img"
+                            style={{width: "75%"}}
+                        />
+                        <h1>{name}</h1>
+                        <p>
+                            <strong>Location:</strong> {location}
+                        </p>
+                        {company &&
+                        <Fragment>
+                            <strong>Company:</strong> {company}
+                        </Fragment>
+                        }
                         <div>
-                            <img
-                                src={avatar_url}
-                                alt={name}
-                                className="round-img"
-                                style={{width: "75%"}}
-                            />
-                            <h1>{name}</h1>
-                            <p>
-                                <strong>Location:</strong> {location}
-                            </p>
-                            {company &&
-                            <Fragment>
-                                <strong>Company:</strong> {company}
-                            </Fragment>
-                            }
-                            <div>
-                                <strong>Hireable:</strong> {' '}
-                                {hireable ? (
-                                    <i className="fas fa-check text-success" />
-                                ) : (
-                                    <i className="fas fa-times-circle text-danger" />
-                                )}
-                            </div>
+                            <strong>Hireable:</strong> {' '}
+                            {hireable ? (
+                                <i className="fas fa-check text-success" />
+                            ) : (
+                                <i className="fas fa-times-circle text-danger" />
+                            )}
                         </div>
                     </div>
-                    <div className="line"/>
-                    <div className="bio card__space">
-                        {bio && (
-                            <div className="my-1">
-                                <h2>Bio</h2>
-                                <p>{bio}</p>
-                            </div>
-                        )}
-
-                        <ul>
-                            <h2>Info</h2>
-                            <li>
-                                {email &&
-                                    <Fragment>
-                                        <strong>Email:</strong> {email}
-                                    </Fragment>
-                                }
-                            </li>
-                            <li>
-                                {blog &&
-                                    <Fragment>
-                                        <strong>Blog:</strong> {' '}
-                                        <a target="_blank"
-                                           rel="noopener noreferrer"
-                                           href={blog}>{blog}</a>
-                                    </Fragment>
-                                }
-                            </li>
-                            <li>
-                                {login &&
-                                    <Fragment>
-                                        <strong>Login:</strong> {login}
-                                    </Fragment>
-                                }
-                            </li>
-                            <li className="my-1">
-                                <div className="badge badge-primary">Followers: {followers}</div>
-                                <div className="badge badge-success">Following: {following}</div>
-                                <div className="badge badge-light">Public Repos: {public_repos}</div>
-                                <div className="badge badge-dark">Public Gists: {public_gists}</div>
-                            </li>
-                        </ul>
-
-                        <h2>Repos</h2>
-                        <Repos repos={repos} />
-
-                        <a href={html_url}
-                           target="_blank"
-                           className="btn btn-dark my-1"
-                           rel="noopener noreferrer"
-                        >
-                            <i className="fab fa-github" aria-hidden="true" /> {' '}
-                            Visit Github profile
-                        </a>
-                    </div>
                 </div>
+                <div className="line"/>
+                <div className="bio card__space">
+                    {bio && (
+                        <div className="my-1">
+                            <h2>Bio</h2>
+                            <p>{bio}</p>
+                        </div>
+                    )}
 
-                <Link to="/" className="btn btn-dark my-1">
-                    <i className="fas fa-arrow-left" /> {' '}
-                    Back to search
-                </Link>
-            </Fragment>
-        );
-    }
-}
+                    <ul>
+                        <h2>Info</h2>
+                        <li>
+                            {email &&
+                                <Fragment>
+                                    <strong>Email:</strong> {email}
+                                </Fragment>
+                            }
+                        </li>
+                        <li>
+                            {blog &&
+                                <Fragment>
+                                    <strong>Blog:</strong> {' '}
+                                    <a target="_blank"
+                                       rel="noopener noreferrer"
+                                       href={blog}>{blog}</a>
+                                </Fragment>
+                            }
+                        </li>
+                        <li>
+                            {login &&
+                                <Fragment>
+                                    <strong>Login:</strong> {login}
+                                </Fragment>
+                            }
+                        </li>
+                        <li className="my-1">
+                            <div className="badge badge-primary">Followers: {followers}</div>
+                            <div className="badge badge-success">Following: {following}</div>
+                            <div className="badge badge-light">Public Repos: {public_repos}</div>
+                            <div className="badge badge-dark">Public Gists: {public_gists}</div>
+                        </li>
+                    </ul>
+
+                    <h2>Repos</h2>
+                    <Repos repos={repos} />
+
+                    <a href={html_url}
+                       target="_blank"
+                       className="btn btn-dark my-1"
+                       rel="noopener noreferrer"
+                    >
+                        <i className="fab fa-github" aria-hidden="true" /> {' '}
+                        Visit Github profile
+                    </a>
+                </div>
+            </div>
+
+            <Link to="/" className="btn btn-dark my-1">
+                <i className="fas fa-arrow-left" /> {' '}
+                Back to search
+            </Link>
+        </Fragment>
+    );
+
+};
+
+// type checking
+User.propTypes = {
+    loading: PropTypes.bool,
+    user: PropTypes.object.isRequired,
+    repos: PropTypes.array.isRequired,
+    getUser: PropTypes.func.isRequired,
+    getUserRepos: PropTypes.func.isRequired
+};
 
 export default User;
