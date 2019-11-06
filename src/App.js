d65e5f9a13b62db318bfeca821a0ca83ab617441
ferhatucar <ucarferhat3@gmail.com>
2019-11-06 16:30:17 +0100

    adds app and index file

---
+++ src/App.js
@@ -0,0 +1,55 @@
+// components or react imports
+import React, {useState, Fragment} from 'react';
+import Navbar from './components/layout/Navbar';
+import Users from './components/users/Users';
+import User from "./components/users/User";
+import Search from "./components/users/Search";
+import Alert from "./components/layout/Alert";
+import About from "./components/pages/About";
+
+// router
+import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
+
+// state
+import GithubState from "./context/github/GithubState";
+
+// styling
+import './App.css';
+
+
+const App = () => {
+    const [alert, setAlert] = useState(null);
+
+    const alertMessage = (msg, type) => {
+        // show alert when no value is passed
+        setAlert({msg, type});
+
+        // remove alert after 5 seconds
+        setTimeout(() => setAlert(null), 5000);
+    };
+
+    return (
+        <GithubState>
+            <Router>
+                <div className="App">
+                    <Navbar title="Github Finder" icon="fab fa-github" />
+                    <div className="container">
+                        <Alert alert={alert} />
+                        <Switch>
+                            <Route exact path="/" render={() => (
+                                <Fragment>
+                                    <Search setAlert={alertMessage} />
+                                    <Users />
+                                </Fragment>
+                            )}/>
+                            <Route exact path="/about" component={About} />
+                            <Route exact path="/user/:login" component={User} />
+                        </Switch>
+                    </div>
+                </div>
+            </Router>
+        </GithubState>
+    );
+};
+
+export default App;
